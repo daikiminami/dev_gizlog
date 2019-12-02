f20df8209e472ab567793064c2ca135551c07123
daikiminami <rondaiten127@gmail.com>
2019-12-02 22:44:32 +0900

    editページのエラーメッセージのif文削除

--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -8,27 +8,21 @@
     {!! Form::input('hidden', 'user_id', Auth::id(), ['class' => 'form-control']) !!}
     <div class="form-group form-size-small {{ $errors->has('reporting_time') ? 'has-error' : '' }}">
       {!! Form::input('date', 'reporting_time', $report->reporting_time->format('Y-m-d'), ['class' => 'form-control']) !!}
-      @if ($errors->has('reporting_time'))
-        <span class="help-block">
-          <strong>{{ $errors->first('reporting_time') }}</strong>
-        </span>
-      @endif
+      <span class="help-block">
+        <strong>{{ $errors->first('reporting_time') }}</strong>
+      </span>
     </div>
     <div class="form-group {{ $errors->has('title') ? 'has-error' : '' }}">
       {!! Form::input('text', 'title', $report->title, ['class' => 'form-control', 'placeholder' => "Title"]) !!}
-      @if ($errors->has('title'))
-        <span class="help-block">
-          <strong>{{ $errors->first('title') }}</strong>
-        </span>
-      @endif
+      <span class="help-block">
+        <strong>{{ $errors->first('title') }}</strong>
+      </span>
     </div>
     <div class="form-group {{ $errors->has('content') ? 'has-error' : '' }}">
-      {!! Form::textarea('content', $report->content, ['class' => 'form-control', 'placeholder' => 'Content', 'rows' => '10', 'cols' => '50']) !!}
-      @if ($errors->has('content'))
-        <span class="help-block">
-          <strong>{{ $errors->first('content') }}</strong>
-        </span>
-      @endif
+      {!! Form::textarea('content', $report->content, ['class' => 'form-control', 'placeholder' => 'Content']) !!}
+      <span class="help-block">
+        <strong>{{ $errors->first('content') }}</strong>
+      </span>
     </div>
     <button type="submit" class="btn btn-success pull-right">Update</button>
     {!! Form::close() !!}
