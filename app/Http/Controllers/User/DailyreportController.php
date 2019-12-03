57b19a4ba0da7c48c7d7ae47032547ea1626d361
daikiminami <rondaiten127@gmail.com>
2019-12-03 10:15:35 +0900

    user_idの更新をコントロール内で処理するように変更

--- app/Http/Controllers/User/DailyreportController.php
+++ app/Http/Controllers/User/DailyreportController.php
@@ -53,6 +53,7 @@ class DailyreportController extends Controller
     public function store(DailyReportRequest $request)
     {
         $input = $request->all();
+        $input['user_id'] = Auth::id();
         $this->report->fill($input)->save();
         return redirect()->route('report.index');
     }
