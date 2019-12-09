b2c02c753d8fbd9041b3d3757f09c526ccf3f897
daikiminami <rondaiten127@gmail.com>
2019-12-09 16:54:31 +0900

    questionとcommentのマイグレーションファイルを作成

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,6 @@ Route::group(['prefix' => '/', 'user.', 'namespace' => 'User'], function () {
     Route::get('attendance/mypage', ['as' => 'attendance.mypage', 'uses' => 'AttendanceController@showMypage']);
 
     Route::resource('report', DailyReportController::class);
-
 });
 
 
