d9526162b70c68c07ee1bd6198ed980fb74f9204
daikiminami <rondaiten127@gmail.com>
2019-12-13 11:30:03 +0900

    コメントモデルの作成

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'question_id',
+        'comment'
+    ];
+
+    protected $dates = 
+    [
+        'created_at'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function question()
+    {
+        return $this->belongsTo(Question::class, 'question_id');
+    }
 }
