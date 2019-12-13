9b8abb3fa6f49fc082972e1f6abaab6dae2995cc
daikiminami <rondaiten127@gmail.com>
2019-12-13 15:09:18 +0900

    コメントテーブルにdeleted_atのカラムを追加

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         'user_id',
         'question_id',
@@ -14,7 +17,8 @@ class Comment extends Model
 
     protected $dates = 
     [
-        'created_at'
+        'created_at',
+        'deleted_at'
     ];
 
     public function user()
